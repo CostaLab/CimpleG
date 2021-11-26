0e76dbc44b8f0066d12cbf2720e232427f221b65
tiagomaie <tiagomaie@hotmail.com>
2021-11-26 18:20:39 +0100

    Worked on a number of changes, CimpleG should now be faster and ligher.
    Implemented a couple of changes that should make CimpleG faster and more
    efficient, particularly when calculating the difference in means and sum
    of variances (the fastest will be when both Rfast and matrixStats are
    installed).
    
    Added butcher::butcher to the end of the workflow of the complex models, which
    should make them much much lighter.
    
    Implemented a way for the user to save the resulting models directly in
    the CimpleG function.
    
    Added a verbosity argument to easily control the output logs.
    
    Added a more simple function to do the diffmeans sumvar plots.
    
    Added functions to save and load data in a faster manner.
    
    Renamed the way we call some of the models (eg when running CimpleG,
    instead of calling the method "parab_scale", we now call it "CimpleG").
    
    Added more documentation for functions that were lacking it.
    
    Added a few tests (parallel test is not working locally but works on the
    cluster).

---
+++ R/CimpleG-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+#' @aliases CimpleG-package
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom dplyr %>%
+#' @importFrom grDevices colorRampPalette
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices pdf
+#' @importFrom grDevices png
+#' @importFrom methods is
+#' @importFrom rlang abort
+#' @importFrom stats predict
+#' @importFrom utils head
+## usethis namespace: end
+NULL
---
+++ R/save_load_helper.R
@@ -0,0 +1,47 @@
+#' Save an R object to disk with fast and efficient compression algorithms.
+#'
+#' @param object Object to be saved to disk.
+#'
+#' @param file_name Name of the file to be saved. Note that the extension
+#'  (e.g. \code{.rds}, \code{.rds.zstd}, \code{.rds.lz4}) will be defined by
+#'  \code{file_format}, so the user does not need to define it.
+#'
+#' @param file_format One of "zstd", "lz4", "gzip", "bzip2","xz", "nocomp".
+#'  \code{zstd} is the best option, fast compression and loading times, low space usage.
+#'
+#' @export
+save_object <- function(object, file_name, file_format=NULL){
+
+  stopifnot(file_format %in% c("zstd", "lz4", "gzip", "bzip2", "xz", "nocomp"))
+
+  if(file_format %in% "nocomp"){
+    saveRDS(object = object, file = paste0(file_name, ".rds"), compress = FALSE)
+    return(invisible(NULL))
+  }
+
+  if(file_format %in% c("zstd", "lz4")){
+    con <- archive::file_write(
+      file = paste0(file_name, ".rds.", file_format),
+      filter = file_format
+    )
+    open(con)
+    saveRDS(object = object, file = con)
+    close(con)
+  }else{
+    saveRDS(object = object, file = paste0(file_name,".rds"), compress = file_format)
+  }
+}
+
+#' Load an R object saved with CimpleG or an RDS file.
+#'
+#' @param file_name File name in the working directory or path to file
+#'  to be loaded. Files saved with \code{CimpleG::save_object} and \code{.rds}
+#'  files are supported. If the \code{archive} package is installed.
+#' @export
+load_object <- function(file_name){
+  con <- archive::file_read(file = file_name)
+  res <- readRDS(file = con)
+  close(con)
+  return(res)
+}
+
