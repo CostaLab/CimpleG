c2117ba3b51da8907f6b6050b5806627ea6aa2b7
tiagomaie <tiagomaie@hotmail.com>
2022-07-25 20:29:17 +0200

    improved logic and fail checks of save_object/load_object functions. now also works more in line with saveRDS

--- R/save_load_helper.R
+++ R/save_load_helper.R
@@ -2,47 +2,54 @@
 #'
 #' @param object Object to be saved to disk.
 #'
-#' @param file_name Name of the file to be saved. Note that the extension
-#'  (e.g. \code{.rds}, \code{.rds.zstd}, \code{.rds.lz4}) will be defined by
-#'  \code{file_format}, so the user does not need to define it.
+#' @param file_name Name of the file where the R object is saved to.
 #'
 #' @param file_format One of "zstd", "lz4", "gzip", "bzip2","xz", "nocomp".
 #'  \code{zstd} is the best option, fast compression and loading times, low space usage.
+#'  Formats "zstd" and "lz4" are only available if package \code{archive} is installed.
 #'
 #' @export
-save_object <- function(object, file_name, file_format=NULL){
+save_object <- function(object, file_name, file_format="zstd"){
 
   stopifnot(file_format %in% c("zstd", "lz4", "gzip", "bzip2", "xz", "nocomp"))
+  stopifnot(length(file_format) == 1)
 
-  if(file_format %in% "nocomp"){
-    saveRDS(object = object, file = paste0(file_name, ".rds"), compress = FALSE)
+  if(file_format %in% c("zstd", "lz4")){
+    if(requireNamespace("archive", quietly = TRUE)){
+      con <- archive::file_write(file = file_name, filter = file_format)
+      open(con)
+      saveRDS(object = object, file = con)
+      close(con)
+    }else{
+      warning("Package 'archive' needs to be installed to compress files in formats 'zstd' and 'lz4'.\n Saving object with default 'saveRDS()' function instead.")
+      saveRDS(object = object, file = file_name, compress = TRUE)
+    }
     return(invisible(NULL))
   }
 
-  if(file_format %in% c("zstd", "lz4")){
-    con <- archive::file_write(
-      file = paste0(file_name, ".rds.", file_format),
-      filter = file_format
-    )
-    open(con)
-    saveRDS(object = object, file = con)
-    close(con)
-  }else{
-    saveRDS(object = object, file = paste0(file_name,".rds"), compress = file_format)
-  }
+  saveRDS(
+    object = object,
+    file = file_name,
+    compress = ifelse(file_format %in% "nocomp", FALSE, file_format)
+  )
 }
 
 #' Load an R object saved with CimpleG or an RDS file.
 #'
 #' @param file_name File name in the working directory or path to file
-#'  to be loaded. Files saved with \code{CimpleG::save_object} and \code{.rds}
-#'  files are supported. If the \code{archive} package is installed.
+#'  to be loaded. Files saved with \code{CimpleG::save_object} and \code{base::saveRDS}
+#'  files are supported.
 #' @export
 load_object <- function(file_name){
   if(!file.exists(file_name)) stop(paste0("File '",file_name,"' not found."))
-  con <- archive::file_read(file = file_name)
-  res <- readRDS(file = con)
-  close(con)
-  return(res)
+
+  if(requireNamespace("archive", quietly = TRUE)){
+    con <- archive::file_read(file = file_name)
+    res <- readRDS(file = con)
+    close(con)
+    return(res)
+  }
+
+  res <- readRDS(file = file_name)
 }
 
