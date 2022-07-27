0a13e3945e66772f2f056f6c72857cdecb270ed7
tiagomaie <tiagomaie@hotmail.com>
2022-07-27 18:09:02 +0200

    adding tests to ensure input data is not messed with, to test if pred_type works properly and to test if using summarizedexp as input works properly

--- tests/testthat/test-CimpleG.R
+++ tests/testthat/test-CimpleG.R
@@ -19,6 +19,30 @@ test_that("signatures and deconv w/ CimpleG", {
   expect_identical(res$signatures, c(CELL_TYPE_MSCORFIBRO="cg03369247", CELL_TYPE_NEURONS="cg24548498"))
 })
 
+test_that("signatures w/ CimpleG when using SummarizedExperiment as input", {
+
+  se_train <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(train_data)),
+    colData = train_targets
+  )
+
+  se_test <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(test_data)),
+    colData = test_targets
+  )
+
+  set.seed(42)
+  res <- CimpleG(
+    train_data = se_train,
+    test_data = se_test,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0
+  )
+
+  expect_identical(res$signatures, c(CELL_TYPE_MSCORFIBRO="cg03369247", CELL_TYPE_NEURONS="cg24548498"))
+})
+
 test_that("signatures are generated when providing single target column", {
   set.seed(42)
   sigs <- CimpleG(
@@ -47,4 +71,151 @@ test_that("signatures are generated when just train_only is set to TRUE", {
     )$signatures
   expect_identical(sigs,c(CELL_TYPE_MSCORFIBRO="cg03369247", CELL_TYPE_NEURONS="cg24548498"))
 })
+
+test_that("only 'hyper' signatures are generated when pred_type is set to 'hyper'", {
+  set.seed(42)
+  res <- CimpleG(
+    train_data = train_data,
+    train_targets = train_targets,
+    test_data = test_data,
+    test_targets = test_targets,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0,
+    pred_type = "hyper"
+    )
+  expect_true(all(res$results$CELL_TYPE_NEURONS$train_res$train_results$pred_type))
+  expect_true(all(res$results$CELL_TYPE_NEURONS$train_res$dt_dmsv$pred_type))
+  expect_true(all(res$results$CELL_TYPE_NEURONS$test_perf$pred_type))
+  expect_true(all(res$results$CELL_TYPE_NEURONS$train_res$train_results$diff_means > 0))
+  expect_true(all(res$results$CELL_TYPE_NEURONS$train_res$dt_dmsv$diff_means > 0))
+  expect_true(all(res$results$CELL_TYPE_NEURONS$test_perf$diff_means > 0))
+
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$train_results$pred_type))
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$dt_dmsv$pred_type))
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$test_perf$pred_type))
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$train_results$diff_means > 0))
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$dt_dmsv$diff_means > 0))
+  expect_true(all(res$results$CELL_TYPE_MSCORFIBRO$test_perf$diff_means > 0))
+})
+
+test_that("only 'hypo' signatures are generated when pred_type is set to 'hypo'", {
+  set.seed(42)
+  res <- CimpleG(
+    train_data = train_data,
+    train_targets = train_targets,
+    test_data = test_data,
+    test_targets = test_targets,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0,
+    pred_type = "hypo"
+    )
+  expect_false(all(res$results$CELL_TYPE_NEURONS$train_res$train_results$pred_type))
+  expect_false(all(res$results$CELL_TYPE_NEURONS$train_res$dt_dmsv$pred_type))
+  expect_false(all(res$results$CELL_TYPE_NEURONS$test_perf$pred_type))
+  expect_false(all(res$results$CELL_TYPE_NEURONS$train_res$train_results$diff_means > 0))
+  expect_false(all(res$results$CELL_TYPE_NEURONS$train_res$dt_dmsv$diff_means > 0))
+  expect_false(all(res$results$CELL_TYPE_NEURONS$test_perf$diff_means > 0))
+
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$train_results$pred_type))
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$dt_dmsv$pred_type))
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$test_perf$pred_type))
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$train_results$diff_means > 0))
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$train_res$dt_dmsv$diff_means > 0))
+  expect_false(all(res$results$CELL_TYPE_MSCORFIBRO$test_perf$diff_means > 0))
+})
+
+test_that("input data is not changed regardless of input format", {
+
+  dat_used <- train_data
+  dat_unused <- train_data
+
+  dattrg_used <- train_targets
+  dattrg_unused <- train_targets
+
+  tdat_used <- test_data
+  tdat_unused <- test_data
+
+  tdattrg_used <- test_targets
+  tdattrg_unused <- test_targets
+
+  set.seed(42)
+  res <- CimpleG(
+    train_data = dat_used,
+    train_targets = dattrg_used,
+    test_data = tdat_used,
+    test_targets = tdattrg_used,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0
+  )
+
+  expect_identical(dat_used,dat_unused)
+  expect_identical(tdat_used,tdat_unused)
+  expect_identical(dattrg_used,dattrg_unused)
+  expect_identical(tdattrg_used,tdattrg_unused)
+
+  dat_used <- as.data.frame(train_data)
+  dat_unused <- as.data.frame(train_data)
+
+  dattrg_used <- as.data.frame(train_targets)
+  dattrg_unused <- as.data.frame(train_targets)
+
+  tdat_used <- as.data.frame(test_data)
+  tdat_unused <- as.data.frame(test_data)
+
+  tdattrg_used <- as.data.frame(test_targets)
+  tdattrg_unused <- as.data.frame(test_targets)
+
+  set.seed(42)
+  res <- CimpleG(
+    train_data = dat_used,
+    train_targets = dattrg_used,
+    test_data = tdat_used,
+    test_targets = tdattrg_used,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0
+  )
+
+  expect_identical(dat_used,dat_unused)
+  expect_identical(tdat_used,tdat_unused)
+  expect_identical(dattrg_used,dattrg_unused)
+  expect_identical(tdattrg_used,tdattrg_unused)
+
+
+  dat_used <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(train_data)),
+    colData = train_targets
+  )
+  dat_unused <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(train_data)),
+    colData = train_targets
+  )
+
+  tdat_used <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(test_data)),
+    colData = test_targets
+  )
+  tdat_unused <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(beta=t(test_data)),
+    colData = test_targets
+  )
+
+  set.seed(42)
+  res <- CimpleG(
+    train_data = se_train,
+    test_data = se_test,
+    method = "CimpleG",
+    target_columns = c("CELL_TYPE_MSCORFIBRO","CELL_TYPE_NEURONS"),
+    verbose=0
+  )
+
+  expect_identical(dat_used,dat_unused)
+  expect_identical(tdat_used,tdat_unused)
+})
+
+
+
 
