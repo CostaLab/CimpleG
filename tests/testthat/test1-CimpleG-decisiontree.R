9765db82c5caf448e7befbe8663b54375d8a6e03
tiagomaie <tiagomaie@hotmail.com>
2025-11-13 14:33:21 +0100

    skipping on CRAN tests that fail due to random seeds

--- tests/testthat/test1-CimpleG-decisiontree.R
+++ tests/testthat/test1-CimpleG-decisiontree.R
@@ -1,19 +1,19 @@
-
 test_that("signatures are generated w/ decision_tree", {
-  skip_on_ci()
+  skip_on_ci() # test might take too long to execute
+  skip_on_cran() # test might fail with different seed
   set.seed(42)
   res <- suppressWarnings(CimpleG(
-    train_data = train_data[,1:100],
+    train_data = train_data[, 1:100],
     train_targets = train_targets,
-    test_data = test_data[,1:100],
+    test_data = test_data[, 1:100],
     test_targets = test_targets,
     method = "decision_tree",
     target_columns = c("blood_cells", "neurons"),
-    verbose=0
+    verbose = 0
   ))
   res <- c(
     res$results$blood_cells$test_perf$accuracy,
     res$results$neurons$test_perf$accuracy
   )
-  expect_equal(res, c(0.77,0.98), tolerance=0.01)
+  expect_equal(res, c(0.77, 0.98), tolerance = 0.01)
 })
--- tests/testthat/test1-CimpleG-nnet.R
+++ tests/testthat/test1-CimpleG-nnet.R
@@ -1,19 +1,19 @@
-
 test_that("signatures are generated w/ nnet", {
-  skip_on_ci()
+  skip_on_ci() # test might take too long to execute
+  skip_on_cran() # test might fail with different seed
   set.seed(42)
   res <- suppressWarnings(CimpleG(
-    train_data = train_data[,1:100],
+    train_data = train_data[, 1:100],
     train_targets = train_targets,
-    test_data = test_data[,1:100],
+    test_data = test_data[, 1:100],
     test_targets = test_targets,
     method = "mlp",
     target_columns = c("blood_cells", "neurons"),
-    verbose=0
+    verbose = 0
   ))
   res <- c(
     res$results$blood_cells$test_perf$accuracy,
     res$results$neurons$test_perf$accuracy
   )
-  expect_equal(res, c(0.9176471,0.9882353), tolerance=0.001)
+  expect_equal(res, c(0.9176471, 0.9882353), tolerance = 0.001)
 })
